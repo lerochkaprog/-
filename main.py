7ede27e5bf33d241dff71e38d50c4e66ee0c3ba3
MOB_master <lera061009@gmail.com>
2022-11-05 21:51:47 +0300

    homeeerok

---
+++ main.py
@@ -0,0 +1,12 @@
+class Class:
+    def __init__(self, lion, elephant):
+        self.lion = lion
+        self.elephant = elephant
+    def __shifr(self):
+        return self.lion + self.elephant
+    def get_shifr(self):
+        pass
+
+
+s = Class(lion='sound lion', elephant='sound elephant')
+print(s)
